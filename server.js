9c30d2bb7db6f8eaeab5f7c609ce1548cd8426cb
miguel <jmiguelcj@gmail.com>
2022-02-17 21:07:59 -0700

    server.js, comment line 8 to 12, add line 13

--- server.js
+++ server.js
@@ -5,11 +5,12 @@ const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 5000; // <== You can change the port
 
 server.use(middlewares);
-server.use(jsonServer.rewriter({
-  // '/api/*': '/$1',
-  '/http://localhost:5000/appointments/*': '/$1',
-  // '/blog/:resource/:id/show': '/:resource/:id'
-}))
+// server.use(jsonServer.rewriter({
+//   // '/api/*': '/$1',
+//   // '/http://localhost:5000/appointments/*': '/$1',
+//   // '/blog/:resource/:id/show': '/:resource/:id'
+// }))
+server.use('/api', router)
 server.use(router);
 
 server.listen(port);
